44d3c2852cd9f3759e908b8b9f52e59a0df25406
Jonathan Vanegas <jonathanvanegasg@hotmail.com>
2024-05-30 00:24:07 -0500

    modificando validaciones form create user

--- src/app/components/form-create-user/form-create-user.component.ts
+++ src/app/components/form-create-user/form-create-user.component.ts
@@ -12,7 +12,6 @@ import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { userCreateInterface } from '../../core/interface/usuario.interface';
-import { passwordRegex } from '../../helpers/validatePassword.helper';
 
 @Component({
   selector: 'app-form-create-user',
@@ -29,6 +28,7 @@ import { passwordRegex } from '../../helpers/validatePassword.helper';
 })
 export class FormCreateUserComponent implements OnInit {
   createUserForm: FormGroup;
+  isCreate: boolean = false;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -36,40 +36,44 @@ export class FormCreateUserComponent implements OnInit {
     private router: Router
   ) {}
 
+  get userForm() {
+    return this.createUserForm.controls;
+  }
+
   ngOnInit(): void {
     this.createUserForm = this.formBuilder.group({
       name: ['', [Validators.required, Validators.minLength(6)]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]],
       role: ['user', Validators.required],
-      clientType: [null, Validators.required],
+      clientType: ['', Validators.required],
     });
   }
 
   onSubmit(): void {
-    Object.values(this.createUserForm.controls).forEach((control) => {
-      control.markAsTouched();
-    });
+    this.isCreate = true;
 
-    if (this.createUserForm.valid) {
-      const data = this.createUserForm.value;
-      const newUser: userCreateInterface = {
-        name: data.name,
-        email: data.email,
-        password: data.password,
-        role: data.role,
-        clientType: data.clientType,
-      };
-
-      this.usersService.createUser(newUser).subscribe(
-        (resp: any) => {
-          console.log(resp);
-          this.router.navigate(['/']);
-        },
-        (error) => {
-          console.error('Error creating user', error);
-        }
-      );
+    if (this.createUserForm.invalid) {
+      return;
     }
+
+    const data = this.createUserForm.value;
+    const newUser: userCreateInterface = {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      role: data.role,
+      clientType: data.clientType,
+    };
+
+    this.usersService.createUser(newUser).subscribe(
+      (resp: any) => {
+        console.log(resp);
+        this.router.navigate(['/']);
+      },
+      (error) => {
+        console.error('Error creating user', error);
+      }
+    );
   }
 }
